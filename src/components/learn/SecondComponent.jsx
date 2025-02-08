f46061186af2c41d8a7a9a1282bf16863d635634
ClowderDev <tranlevinhbuu@gmail.com>
2025-02-08 15:17:53 +0700

    29

---
+++ src/components/learn/SecondComponent.jsx
@@ -0,0 +1,10 @@
+const SecondComponent = () => {
+  return <div>Second Component</div>;
+};
+
+const ThirdComponent = () => {
+  return <div>Third Component</div>;
+}
+
+
+export {SecondComponent, ThirdComponent};
