62d1cc467e8a408dda8f888ab9b48d71ea3dbb7c
ClowderDev <tranlevinhbuu@gmail.com>
2025-02-08 16:54:56 +0700

    31

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -2,8 +2,14 @@
 import './style.css';
 
 const MyComponent = () => {
+  // const clowder = [1,2,3];
+  const clowder = {
+    name: 'cat',
+    age: 3
+  }
   return (
     <>
+      <div>{JSON.stringify(clowder)}</div>
       <div>react component</div>
       <div className='child'>child</div>
     </>
