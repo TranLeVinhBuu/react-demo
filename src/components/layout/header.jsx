d16075790605b7fd8a47e91ac8cbe56ab20af55d
ClowderDev <tranlevinhbuu@gmail.com>
2025-02-09 19:31:01 +0700

    49

--- src/components/layout/header.jsx
+++ src/components/layout/header.jsx
@@ -1,12 +1,21 @@
-import './header.css';
+import "./header.css";
+import { Link } from "react-router-dom";
 const Header = () => {
-    return (
-        <ul>
-            <li><a class="active" href="/">Home</a></li>
-            <li><a href="/users">Users</a></li>
-            <li><a href="/product">Products</a></li>
-        </ul>
-    )
-}
+  return (
+    <ul>
+      <li>
+        <Link class="active" to="/">
+          Home
+        </Link>
+      </li>
+      <li>
+        <Link to="/users">Users</Link>
+      </li>
+      <li>
+        <Link to="/product">Products</Link>
+      </li>
+    </ul>
+  );
+};
 
 export default Header;
