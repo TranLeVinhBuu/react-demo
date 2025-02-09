5dee1e6022853b4854a3754921b523c1fcb7eafc
ClowderDev <tranlevinhbuu@gmail.com>
2025-02-09 19:27:35 +0700

    48

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import ReactLogo from "./assets/react.svg";
 import React, { useState } from "react";
 import Header from "./components/layout/header";
 import Footer from "./components/layout/footer";
+import { Outlet } from "react-router-dom";
 
 const App = () => {
   const [todoList, setTodoList] = useState([]);
@@ -41,6 +42,7 @@ const App = () => {
           </div>
         )}
       </div>
+      <Outlet />
       <Footer />
     </>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -1,42 +1,40 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from 'react-router-dom';
-import LoginPage from './pages/login.jsx';
-import RegisterPage from './pages/register.jsx';
-import ProductPage from './pages/product.jsx';
-import UserPage from './pages/users.jsx';
-import './styles/global.css';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import LoginPage from "./pages/login.jsx";
+import RegisterPage from "./pages/register.jsx";
+import ProductPage from "./pages/product.jsx";
+import UserPage from "./pages/users.jsx";
+import "./styles/global.css";
 
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <App />,
+    children: [
+      {
+        path: "/product",
+        element: <ProductPage />,
+      },
+      {
+        path: "/users",
+        element: <UserPage />,
+      },
+    ],
   },
   {
-    path: '/login',
+    path: "/login",
     element: <LoginPage />,
   },
   {
-    path: '/register',
+    path: "/register",
     element: <RegisterPage />,
   },
-  {
-    path: '/product',
-    element: <ProductPage />,
-  },
-  {
-    path: '/users',
-    element: <UserPage />,
-  }
-])
+]);
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router}/>
-  </StrictMode>,
-)
-  
+    <RouterProvider router={router} />
+  </StrictMode>
+);
