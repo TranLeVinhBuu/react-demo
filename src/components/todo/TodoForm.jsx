9e5042cfc1e5b7ebb7542a85b33c05a0852494ed
ClowderDev <tranlevinhbuu@gmail.com>
2025-02-09 09:42:20 +0700

    36

--- src/components/todo/TodoForm.jsx
+++ src/components/todo/TodoForm.jsx
@@ -1,10 +1,23 @@
 const TodoForm = (props) => {
-  const {addNewTodo} = props;
-  addNewTodo('clowder')
+  // const {addNewTodo} = props;
+  // addNewTodo('clowder')
+
+  const handleClick = () => {
+    console.log('clicked')
+  }
+
+  const handleChange = (e) => {
+    console.log('handle change', e)
+  }
+
   return (
      <div className="todo-form">
-        <input type="text"/>
-        <button>Add</button>
+        <input type="text"
+        onChange={(event) => handleChange(event.target.value) }/>
+        <button 
+        style={{cursor: "pointer"}}
+        onClick={handleClick}
+        >Add</button>
       </div>
   )
 }
